bfe1a9b62cc050094d65a5c48b35261eea118525
Jonathan-md <jonathanpercamamani@gmail.com>
2025-07-09 19:45:02 -0500

    Revert "Validación de n y mejora de interfaz "

--- Ejercicio3.cpp
+++ Ejercicio3.cpp
@@ -5,13 +5,9 @@ using namespace std;
 int main(){
 
     int n, A[10], aux;
-    cout << "Cuantos numeros quieres ingresar? (maximo 10): ";
-    cin >> n;
-    if (n > 10 || n <= 0) {
-        cout << "Numero invalido. Debe ser entre 1 y 10." << endl;
-        return 1; 
-    }
-    cout << "Ingresa los " << n << " numeros:" << endl;
+
+    cin>>n;
+
     for(int i=0; i<n; i++){
         cin>>A[i];
     }
@@ -20,7 +16,6 @@ int main(){
         A[i] = A[n-(i+1)];
         A[n-(i+1)] = aux;
     }
-    cout << "Arreglo invertido: ";
     for(int i=0; i<n; i++){
         cout << A[i];
     }
